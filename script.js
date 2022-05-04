3e3aec30eb0924832015bf7a977b8003a02e951a
kiselmen <kiselmen@rambler.ru>
2022-05-04 14:46:07 +0300

    feat: keyboard can switch upper(lower) case by caps and shift, change language by ctl+alt, can print a non functional keys, show pressed up(down) keys

--- script.js
+++ script.js
@@ -13,6 +13,10 @@ class myKeyboard {
         } else {
           this.language = 'en';
         }
+        localStorage.setItem('language', this.language);
+        this.isUpperCase = false;
+        this.isCaps = false;
+        this.isCtr = false;
     }
 
     initialize(){
@@ -44,7 +48,7 @@ class myKeyboard {
         })
         this.keyboard.append(keyboardRow);
       })
-      console.log(this.allKeys);
+      
       this.info.classList.add('info');
       this.info.textContent ='To switch ENG/РУС, press Ctrl+Alt on Windows/Linux or Cmd+Alt on Mac.';
   
@@ -55,14 +59,158 @@ class myKeyboard {
   
       document.body.append(this.mainContainer);
       
-      this.showSymbols(this.language)
+      this.showSymbols(this.language);
+
+      this.addActivity();
     }
 
-    showSymbols(language = this.language){
+    showSymbols(language = this.language, upper = this.isUpperCase){
       this.keyboard.querySelectorAll('.keyboard-key').forEach( (key) =>{
-        console.log(key);
-        key.textContent = this.allKeys[key.dataset.keyCode].writing[language];
+        if (upper) {
+            if (this.allKeys[key.dataset.keyCode].hasFunction) {
+                key.textContent = this.allKeys[key.dataset.keyCode].writing[language];
+            } else {
+                key.textContent = this.allKeys[key.dataset.keyCode].writing[language].toUpperCase();
+            }
+        } else {
+            key.textContent = this.allKeys[key.dataset.keyCode].writing[language];
+        }
       })
-  }
 
+    }
+
+    addActivity(){
+
+      this.textContainer.focus();
+
+      this.textContainer.addEventListener('blur', () => {
+          this.textContainer.focus();
+      });
+  
+      document.addEventListener('keydown', (event) => {
+        event.stopImmediatePropagation();
+        event.preventDefault();
+
+        let keyPress = null;
+        document.querySelectorAll('.keyboard-key').forEach( (key) => {
+            if (key.dataset.keyCode === event.code) {
+              keyPress = key;
+            }
+        })
+
+        if (keyPress) {
+            keyPress.classList.add('active');
+        } else {
+            return;
+        }
+
+        if (!this.allKeys[keyPress.dataset.keyCode].hasFunction) {
+          this.insertText(this.allKeys[keyPress.dataset.keyCode].writing[this.language]);
+        } else {
+            if (event.code === 'CapsLock'){
+              this.isUpperCase = !this.isUpperCase;
+              this.isCaps = !this.isCaps;
+              this.showSymbols(this.language, this.isUpperCase);
+            }  
+            else if ((event.code === 'ShiftLeft')||(event.code === 'ShiftRight')){
+              if (!event.repeat){
+                this.isUpperCase = !this.isUpperCase;
+                this.showSymbols(this.language, this.isUpperCase);
+              }
+            }
+            else if ((event.code === 'ControlLeft')||(event.code === 'ControlRight')){
+              if (!event.repeat){
+                  this.isCtr = !this.isCtr;
+              }
+            }
+            else if ((event.code === 'AltLeft')||(event.code === 'AltRight')){
+              if (!event.repeat){
+                  if (this.isCtr) {
+                      if (this.language === 'en') {
+                        this.language = 'ru';
+                      } else {
+                        this.language = 'en';
+                      }
+                      localStorage.setItem('language', this.language);
+                      this.showSymbols(this.language, this.isUpperCase);
+                  }
+              }
+            }
+        }
+      })
+
+      document.addEventListener('keyup', (event) => {
+        event.stopImmediatePropagation();
+        event.preventDefault();
+        
+        let keyPress = null;
+        document.querySelectorAll('.keyboard-key').forEach( (key) => {
+            if (key.dataset.keyCode === event.code) {
+              keyPress = key;
+            }
+        })
+        if (!keyPress) return;
+
+        if (event.code === 'CapsLock') {
+            if (!this.isCaps) {
+              setTimeout(() => {
+                keyPress.classList.remove('active');
+              }, 50);
+            }
+        } 
+        else if ((event.code === 'ShiftLeft')||(event.code === 'ShiftRight')) {
+            this.isUpperCase = !this.isUpperCase;
+            this.showSymbols(this.language, this.isUpperCase);
+            setTimeout(() => {
+                keyPress.classList.remove('active');
+            }, 50);
+        } 
+        else if ((event.code === 'ControlLeft')||(event.code === 'ControlRight')){
+            this.isCtr = !this.isCtr;
+            setTimeout(() => {
+              keyPress.classList.remove('active');
+            }, 50);
+        }
+        else {
+          setTimeout(() => {
+            keyPress.classList.remove('active');
+          }, 50);
+        } 
+      })
+
+      this.keyboard.addEventListener('click', (event) => {
+        if (event.target.classList.contains('keyboard-key')){
+          this.textContainer.focus();
+
+          const eventHoldDown = new KeyboardEvent('keydown', {
+            bubbles: true,
+            cancelable: true,
+            code: event.target.dataset.keyCode,
+            view: window,
+          });
+
+          document.dispatchEvent(eventHoldDown);
+    
+          const eventReleased = new KeyboardEvent('keyup', {
+            bubbles: true,
+            cancelable: true,
+            code: event.target.dataset.keyCode,
+            view: window,
+          });
+
+          document.dispatchEvent(eventReleased);
+        }
+      });
+  
+    }
+
+    insertText(chars) {
+      console.log(this.textContainer.selectionStart, this.textContainer.selectionEnd);
+      if (this.isUpperCase) {
+          this.textContainer.value = this.textContainer.value + chars.toUpperCase();
+      } else {
+        this.textContainer.value = this.textContainer.value + chars;
+      }
+    }
+  
 }
